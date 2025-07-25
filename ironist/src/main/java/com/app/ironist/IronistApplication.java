946bef8dacd460ab3c7cde5f9f5d2c68f74e2ab9
rocket70001 <rocket70001@gmail.com>
2025-07-25 18:35:31 +0900

    chore: setup Spring Boot project with basic dependencies

---
+++ ironist/src/main/java/com/app/ironist/IronistApplication.java
@@ -0,0 +1,13 @@
+package com.app.ironist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IronistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IronistApplication.class, args);
+	}
+
+}
---
+++ ironist/src/test/java/com/app/ironist/IronistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.ironist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IronistApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
