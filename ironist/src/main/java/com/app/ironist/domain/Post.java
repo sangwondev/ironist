aa34150394dcc6f5e1948d77992a718726b5a8f7
sangwondev <sangwonh34@gmail.com>
2025-07-27 00:03:27 +0900

    feat: add charCount and writingTime fields to Post entity

--- ironist/src/main/java/com/app/ironist/domain/Post.java
+++ ironist/src/main/java/com/app/ironist/domain/Post.java
@@ -19,6 +19,8 @@ public class Post {
     private String content;
     private Boolean isPublic;
     private LocalDateTime createdDatetime;
+    private Integer charCount;
+    private Integer writingTime;
 
     @ManyToOne
     @JoinColumn(name = "member_id")
